c2f7e3795d81a0cfafe49b79846bf96daa8811b1
origin1508 <origin1508@gmail.com>
2022-08-22 18:30:10 +0900

    FEAT: Certificates 컴포넌트 생성

---
+++ front/src/components/certificate/Certificates.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect }  from "react";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import * as Api from "api";
+import Certificate from "./Certificate";
+
+function Certificates({ portfolioOwnerId, isEditable }) {
+    const [certificates, setCertificates] = useState([]);
+    const [isAdding, setIsAdding] = useState(false);
+
+    useEffect(() => {
+        // Api.get("certificates", portfolioOwnerId).then(res => setCertificates(res.data))
+    }, [portfolioOwnerId]);
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>자격증</Card.Title>
+                {certificates.map(certificate => (
+                    <Certificate
+                        key={certificate.id}
+                        certificate={certificate}
+                        setCertificates={setCertificates}
+                        isEditable={isEditable}
+                    />
+                ))}
+                {isEditable && (
+                    <Row className="mt-3 text-center mb-4">
+                        <Col sm={{ span: 20 }}>
+                            <Button onClick={() => setIsAdding(true)}>+</Button>
+                        </Col>
+                    </Row>
+                )}
+                {isAdding && (
+                    <CertificateAddForm 
+                        portfolioOwnerId={portfolioOwnerId}
+                        setCertificates={setCertificates}
+                        setIsAdding={setIsAdding}
+                    />
+                )}
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Certificates;
