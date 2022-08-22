f3ae8dd8ece079a708f610af17ccbb65585e9410
dlzagu <eodnsdlekd@naver.com>
2022-08-22 18:43:10 +0900

    FIX: api 제거

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import EducationCard from "./EducationCard";
 
-function Education({ education, setEducations }) {
+function Education({ isEditable, education, setEducations }) {
   // useState 훅을 통해 isEditing 상태를 생성함.
-  const [isEditing, setIsEditing] = useState(falase);
+  const [isEditing, setIsEditing] = useState(false);
   // useState 훅을 통해 education 상태를 생성함.
-
-  useEffect(() => {
-    // 'education/유저id' 엔드포인트로 GET 요청을 하고, education을 response의 data로 세팅함.
-    Api.get("users", portfolioOwnerId).then((res) => setEducation(res.data));
-  }, [portfolioOwnerId]);
 
   return (
     <>
       {isEditing ? (
-        <EducationEditForm
-          education={education}
-          setEducations={setEducations}
-          setIsEditing={setIsEditing}
-        />
+        ""
       ) : (
+        // <EducationEditForm
+        //   education={education}
+        //   setEducations={setEducations}
+        //   setIsEditing={setIsEditing}
+        // />
         <EducationCard
           education={education}
           setEducations={setEducations}
