bdd16737ec072ee4960071bd164885d8bd8b65e8
foxmon <jjockrod@naver.com>
2022-08-22 15:21:05 +0900

    FEAT: AwardCard 추가

---
+++ front/src/components/award/AwardCard.js
@@ -0,0 +1,33 @@
+import { Card, Col, Row, Button } from "react-bootstrap"
+
+const AwardCard = ({ award, isEditable, onEnterEditModeButtonClickEvent }) => {
+    const onEnterEditModeButtonClickEventHandler = () => {
+        onEnterEditModeButtonClickEvent()
+    }
+
+    return (
+        <Card.Text>
+            <Row className="align-items-center">
+                <Col>
+                    <span>{award.title}</span>
+                    <br />
+                    <span className="text-muted">{award.description}</span>
+                </Col>
+                {isEditable && (
+                    <Col xs lg="1">
+                        <Button
+                            variant="outline-info"
+                            size="sm"
+                            onClick={onEnterEditModeButtonClickEventHandler}
+                            className="mr-3"
+                        >
+                            편집
+                        </Button>
+                    </Col>
+                )}
+            </Row>
+        </Card.Text>
+    )
+}
+
+export default AwardCard
