0bbe656ca05aeba7ea68bd6b82a07af38ac41298
foxmon <jjockrod@naver.com>
2022-08-22 15:21:26 +0900

    FEAT: AwardCardPresenter 추가

---
+++ front/src/components/award/AwardCardPresenter.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react"
+import AwardCard from "./AwardCard"
+import AwardCardEdit from "./AwardCardEdit"
+
+const AwardCardPresenter = ({ award, isEditable, onEditButtonClickEvent }) => {
+    const [isEditMode, setIsEditMode] = useState(false)
+
+    const onAwardEditButtonClickEventHandler = (editedAward) => {
+        onEditButtonClickEvent(editedAward)
+    }
+
+    const onCancelButtonClickEventHandler = (isCanceld) => {
+        setIsEditMode(isCanceld)
+    }
+
+    const onEnterEditModeButtonClickEventHandler = () => {
+        setIsEditMode((prev) => !prev)
+    }
+
+    return (
+        <React.Fragment>
+            {isEditMode ? (
+                <AwardCardEdit
+                    award={award}
+                    onAwardEditButtonClickEvent={
+                        onAwardEditButtonClickEventHandler
+                    }
+                    onCancelButtonClickEvent={onCancelButtonClickEventHandler}
+                />
+            ) : (
+                <AwardCard
+                    award={award}
+                    isEditable={isEditable}
+                    onEnterEditModeButtonClickEvent={
+                        onEnterEditModeButtonClickEventHandler
+                    }
+                />
+            )}
+        </React.Fragment>
+    )
+}
+
+export default AwardCardPresenter
