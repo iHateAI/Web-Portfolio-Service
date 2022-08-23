9804347c86815161a14f1c1497de6f92090f0635
origin1508 <origin1508@gmail.com>
2022-08-23 10:58:31 +0900

    FIX: CeritificateAddForm 수정
    날짜 입력칸 추가, 각 요소 마진수정, Form onSubmit 핸들러 추가

--- front/src/components/certificate/CertificateAddForm.js
+++ front/src/components/certificate/CertificateAddForm.js
@@ -1,32 +1,47 @@
 import React, { useState } from "react";
 import { Button, Form, Col, Row } from "react-bootstrap";
+import DatePicker from "react-datepicker";
 import * as Api from "../../api";
-
+import * as Db from "./db";
 
 function CertificateAddForm({ portfolioOwnerId, setCertificates, setIsAdding }) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [date, setDate] = useState(new Date());
+    const [certificateDate, setCertificateDate] = useState(new Date());
+
+    const formatDate = (date) => {
+        const yyyy = date.getFullYear();
+        const mm = date.getMonth() + 1;
+        const dd = date.getDate();
+
+        return yyyy + '-' + mm + '-' + dd
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const user_id = portfolioOwnerId;
+        // const user_id = portfolioOwnerId;
 
-        await Api.post("certificates/create", {
-            user_id: portfolioOwnerId,
+        // await Api.post("certificates/create", {
+        //     user_id: portfolioOwnerId,
+        //     title,
+        //     description,
+        // });
+
+        // const res = await Api.get("certificates", user_id);
+        
+        // post test
+        const res = Db.post(1, {
             title,
             description,
-        });
-
-        const res = await Api.get("certificates", user_id);
-
+            certificateDate: formatDate(certificateDate)
+        })
         setCertificates(res.data);
 
         setIsAdding(false);
     }
     return (
-        <Form >
+        <Form onSubmit={handleSubmit}>
             <Form.Group controlId="certificateAddTitle">
                 <Form.Control
                     type="text"
@@ -35,7 +50,7 @@ function CertificateAddForm({ portfolioOwnerId, setCertificates, setIsAdding }) 
                     onChange={(e) => setTitle(e.target.value)}
                 />
             </Form.Group>
-            <Form.Group controlId="certificateAddDescripiton">
+            <Form.Group controlId="certificateAddDescripiton" className="mt-3">
                 <Form.Control
                     type="text"
                     value={description}
@@ -43,9 +58,12 @@ function CertificateAddForm({ portfolioOwnerId, setCertificates, setIsAdding }) 
                     onChange={(e) => setDescription(e.target.value)}
                 />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="certificateAddDate"  className="mt-3">
+                <DatePicker selected={certificateDate} onChange={(date) => setCertificateDate(date)} />
+            </Form.Group>
+            <Form.Group as={Row} className="mt-3 text-center">
                 <Col sm={{ span: 20 }}>
-                    <Button variant="primary" type="submit">확인</Button>
+                    <Button variant="primary" type="submit" className="me-3">확인</Button>
                     <Button variant="secondary" onClick={() => setIsAdding(false)}>취소</Button>
                 </Col>
             </Form.Group>
