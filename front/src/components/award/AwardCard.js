92bb4414eab7399a8293b7a202c6e3b7ae1b4547
foxmon <jjockrod@naver.com>
2022-08-23 11:30:41 +0900

    Feat: AwardCard 삭제 버튼 및 기능 추가

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,8 +1,17 @@
 import { Card, Col, Row, Button } from "react-bootstrap"
 
-const AwardCard = ({ award, isEditable, onEnterEditModeButtonClickEvent }) => {
+const AwardCard = ({
+    award,
+    isEditable,
+    onEnterEditModeButtonClickEvent,
+    onDeleteButtonClickEvent,
+}) => {
     const onEnterEditModeButtonClickEventHandler = () => {
         onEnterEditModeButtonClickEvent()
+    }
+
+    const onDeleteButtonClickEventHandler = () => {
+        onDeleteButtonClickEvent(award)
     }
 
     return (
@@ -22,6 +31,14 @@ const AwardCard = ({ award, isEditable, onEnterEditModeButtonClickEvent }) => {
                             className="mr-3"
                         >
                             편집
+                        </Button>
+                        <Button
+                            variant="danger"
+                            size="sm"
+                            onClick={onDeleteButtonClickEventHandler}
+                            className="mr-3"
+                        >
+                            삭제
                         </Button>
                     </Col>
                 )}
