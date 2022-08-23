4481d66f52c7b52e19c7499da1338ea73ad7abbf
foxmon <jjockrod@naver.com>
2022-08-24 00:18:52 +0900

    Fix: Certificate TestData로 통합 및 수정

--- front/src/components/certificate/CertificateEditForm.js
+++ front/src/components/certificate/CertificateEditForm.js
@@ -1,26 +1,35 @@
-import React, { useState } from "react";
-import { Button, Form, Col, Row } from "react-bootstrap";
-import DatePicker from "react-datepicker";
-import * as Api from "../../api";
-import * as Db from "./db";
+import React, { useState } from "react"
+import { Button, Form, Col, Row } from "react-bootstrap"
+import DatePicker from "react-datepicker"
+// import * as Api from "../../api";
+// import * as Db from "./db";
+import TestData from "../../dev/testData"
 
-function CertificateEditForm({ currentCertificate, setCertificates, setIsEditing }) {
-    const [title, setTitle] = useState(currentCertificate.title);
-    const [description, setDescription] = useState(currentCertificate.description);
-    const [certificateDate, setCertificateDate] = useState(new Date(currentCertificate.certificateDate));
+function CertificateEditForm({
+    currentCertificate,
+    setCertificates,
+    setIsEditing,
+}) {
+    const [title, setTitle] = useState(currentCertificate.title)
+    const [description, setDescription] = useState(
+        currentCertificate.description
+    )
+    const [certificateDate, setCertificateDate] = useState(
+        new Date(currentCertificate.certificateDate)
+    )
 
     const formatDate = (date) => {
-        const yyyy = date.getFullYear();
-        const mm = date.getMonth() + 1;
-        const dd = date.getDate();
+        const yyyy = date.getFullYear()
+        const mm = date.getMonth() + 1
+        const dd = date.getDate()
 
-        return yyyy + '-' + mm + '-' + dd
+        return yyyy + "-" + mm + "-" + dd
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        const user_id = currentCertificate.user_id;
+        const user_id = currentCertificate.user_id
 
         // await Api.put(`certificates/${currentCertificate.id}`, {
         //     user_id,
@@ -31,17 +40,26 @@ function CertificateEditForm({ currentCertificate, setCertificates, setIsEditing
         // const res = await Api.get("certificates", user_id);
 
         // put test
-        const res = Db.put(currentCertificate.id, {
+        // const res = Db.put(currentCertificate.id, {
+        //     user_id,
+        //     id: 1,
+        //     title,
+        //     description,
+        //     certificateDate: formatDate(certificateDate)
+        // })
+
+        const res = await TestData.updateCertificate(TestData.userId, {
             user_id,
-            id: 1,
+            id: currentCertificate.id,
             title,
             description,
-            certificateDate: formatDate(certificateDate)
+            certificateDate: formatDate(certificateDate),
         })
 
-        setCertificates(res.data);
+        // setCertificates(res.data);
+        setCertificates(res)
 
-        setIsEditing(false);
+        setIsEditing(false)
     }
     return (
         <Form onSubmit={handleSubmit}>
@@ -61,9 +79,13 @@ function CertificateEditForm({ currentCertificate, setCertificates, setIsEditing
                     onChange={(e) => setDescription(e.target.value)}
                 />
             </Form.Group>
-            <Form.Group as={Row} controlId="certificateEditDate" className="mt-3">
+            <Form.Group
+                as={Row}
+                controlId="certificateEditDate"
+                className="mt-3"
+            >
                 <Col xs="auto">
-                    <DatePicker 
+                    <DatePicker
                         selected={certificateDate}
                         onChange={(date) => setCertificateDate(date)}
                         dateFormat="yyyy-MM-dd"
@@ -72,12 +94,19 @@ function CertificateEditForm({ currentCertificate, setCertificates, setIsEditing
             </Form.Group>
             <Form.Group as={Row} className="mt-3 text-center mb-4">
                 <Col sm={{ span: 20 }}>
-                    <Button variant="primary" type="submit" className="me-3">확인</Button>
-                    <Button variant="secondary" onClick={() => setIsEditing(false)}>취소</Button>
+                    <Button variant="primary" type="submit" className="me-3">
+                        확인
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={() => setIsEditing(false)}
+                    >
+                        취소
+                    </Button>
                 </Col>
             </Form.Group>
         </Form>
-    );
+    )
 }
 
-export default CertificateEditForm;
+export default CertificateEditForm
