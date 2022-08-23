45ffe4cfe66ae0d32223172def9915fd4d709c66
foxmon <jjockrod@naver.com>
2022-08-24 01:01:10 +0900

    Feat: validation util 추가

---
+++ front/src/util/validation/validation.js
@@ -0,0 +1,20 @@
+class Validation {
+    // Check validation form
+
+    // Award
+    // award의 title과 description의 길이는 앞 뒤 공백 제거 기준 5글자 이상이어야 한다.
+    static validateAwardTitle(title) {
+        const checkTitle = title.trim()
+        if (checkTitle.length < 5) return false
+        return true
+    }
+
+    static validateAwardDescription(description) {
+        const checkDescription = description.trim()
+        if (checkDescription.length < 5) return false
+        return true
+    }
+}
+
+Object.freeze(Validation)
+export default Validation
