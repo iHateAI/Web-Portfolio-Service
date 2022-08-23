1dbf0759cd68601675c95d29b3a1ec17e7f2396a
foxmon <jjockrod@naver.com>
2022-08-24 01:03:30 +0900

    Feat: AwardCardEdit validation 추가

--- front/src/components/award/AwardCardEdit.js
+++ front/src/components/award/AwardCardEdit.js
@@ -1,5 +1,9 @@
-import { useState, useCallback } from "react"
+import React, { useState, useCallback } from "react"
+import useModal from "../../hooks/useModal"
+import useAwardValidation from "../../hooks/useAwardValidation"
+
 import { Form, Row, Col, Button } from "react-bootstrap"
+import AlertModal from "../modal/AlertModal"
 
 const AwardCardEdit = ({
     award,
@@ -9,8 +13,28 @@ const AwardCardEdit = ({
     const [awardTitle, setAwardTitle] = useState(award.title)
     const [awardDescription, setAwardDescription] = useState(award.description)
 
+    const [
+        isShow,
+        onShowButtonClickEventHandler,
+        onCloseButtonClickEventHandler,
+    ] = useModal(false)
+
+    const [
+        checkValidationTitle,
+        checkValidationDescription,
+        checkValidationAll,
+    ] = useAwardValidation()
+
+    const isValidTitle = checkValidationTitle(awardTitle)
+    const isValidDescription = checkValidationDescription(awardDescription)
+    const isValid = checkValidationAll(isValidTitle, isValidDescription)
+
     const onSubmitEventHander = (e) => {
         e.preventDefault()
+        if (!isValid) {
+            onShowButtonClickEventHandler()
+            return
+        }
         const editedAward = {
             id: award.id,
             title: awardTitle,
@@ -32,37 +56,58 @@ const AwardCardEdit = ({
     }
 
     return (
-        <Form onSubmit={onSubmitEventHander}>
-            <Form.Group controlled="formAwardTitle">
-                <Form.Control
-                    type="text"
-                    placeholder="수상내역"
-                    value={awardTitle}
-                    onChange={onAwardTitleChangeEventHandler}
-                />
-            </Form.Group>
-            <Form.Group controlled="formAwardDescription">
-                <Form.Control
-                    type="text"
-                    placeholder="수상내역 설명"
-                    value={awardDescription}
-                    onChange={onAwardDescriptionChangeEventHandler}
-                />
-            </Form.Group>
-            <Form.Group as={Row} className="mt-5 text-center mb-5">
-                <Col sm={colStyle}>
-                    <Button variant="primary" type="submit" className="me-3">
-                        확인
-                    </Button>
-                    <Button
-                        variant="secondary"
-                        onClick={onCancelButtonClickEventHandler}
-                    >
-                        취소
-                    </Button>
-                </Col>
-            </Form.Group>
-        </Form>
+        <React.Fragment>
+            <Form onSubmit={onSubmitEventHander}>
+                <Form.Group controlled="formAwardTitle">
+                    <Form.Control
+                        type="text"
+                        placeholder="수상내역"
+                        value={awardTitle}
+                        onChange={onAwardTitleChangeEventHandler}
+                    />
+                    {!isValidTitle && (
+                        <Form.Text className="text-danger">
+                            수상내역이 올바르지 않습니다.
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group controlled="formAwardDescription">
+                    <Form.Control
+                        type="text"
+                        placeholder="수상내역 설명"
+                        value={awardDescription}
+                        onChange={onAwardDescriptionChangeEventHandler}
+                    />
+                    {!isValidDescription && (
+                        <Form.Text className="text-danger">
+                            수상내역 설명이 올바르지 않습니다.
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group as={Row} className="mt-5 text-center mb-5">
+                    <Col sm={colStyle}>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            className="me-3"
+                        >
+                            확인
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            onClick={onCancelButtonClickEventHandler}
+                        >
+                            취소
+                        </Button>
+                    </Col>
+                </Form.Group>
+            </Form>
+            <AlertModal
+                msg="편집 입력이 올바르지 않습니다."
+                isShow={isShow}
+                onCloseButtonClickEvent={onCloseButtonClickEventHandler}
+            />
+        </React.Fragment>
     )
 }
 
