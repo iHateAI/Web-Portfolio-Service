72110612db060d15d8fba5cf8631cda77119087a
DaHyunOhe <peelsob4d@gmail.com>
2022-08-24 19:27:57 +0900

    FEAT: ProjectCard에 ConfirmModal 연결

--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,23 +1,31 @@
-import React from "react"
-import { Button, ButtonGroup, Card, Row } from "react-bootstrap"
+import React from 'react';
+import { Button, ButtonGroup, Card, Row } from 'react-bootstrap';
 // import { deleteProject } from './dev/mockApiProject';
-import TestData from "../../dev/testData"
+import TestData from '../../dev/testData';
+import useModal from '../../hooks/useModal';
+import ConfirmModal from '../modal/ConfirmModal';
 
 function ProjectCard({ project, isEditable, editClick, getUser }) {
-    const handleDeleteProject = async () => {
+    const [
+        isShow,
+        onShowButtonClickEventHandler,
+        onCloseButtonClickEventHandler,
+    ] = useModal(false);
+
+    const handleDeleteProject = async (bool) => {
         // await deleteProject(project.key);
-        await TestData.deleteProject(project.key)
-        getUser()
-    }
+        await TestData.deleteProject(project._id);
+        getUser();
+    };
 
     return (
         <div className="d-flex justify-content-between align-items-center">
             <Row>
                 <Card.Body>
-                    <Card.Title>{project.name}</Card.Title>
-                    <Card.Subtitle>{project.description}</Card.Subtitle>
+                    <Card.Title>{project.title}</Card.Title>
+                    <Card.Subtitle>{project.detail}</Card.Subtitle>
                     <Card.Text>
-                        {project.date.start} ~ {project.date.end}
+                        {project.startDate} ~ {project.endDate}
                     </Card.Text>
                 </Card.Body>
             </Row>
@@ -27,7 +35,7 @@ function ProjectCard({ project, isEditable, editClick, getUser }) {
                         variant="outline-info"
                         size="sm"
                         style={{
-                            height: "30px",
+                            height: '30px',
                         }}
                         onClick={editClick}
                     >
@@ -37,16 +45,22 @@ function ProjectCard({ project, isEditable, editClick, getUser }) {
                         variant="outline-danger"
                         size="sm"
                         style={{
-                            height: "30px",
+                            height: '30px',
                         }}
-                        onClick={handleDeleteProject}
+                        onClick={onShowButtonClickEventHandler}
                     >
                         삭제
                     </Button>
                 </ButtonGroup>
             )}
+            <ConfirmModal
+                isShow={isShow}
+                onCloseButtonClickEvent={onCloseButtonClickEventHandler}
+                onCheckButtonClickEvent={handleDeleteProject}
+                msg={`${project.title}(을)를 목록에서 삭제하시겠습니까?`}
+            />
         </div>
-    )
+    );
 }
 
-export default ProjectCard
+export default ProjectCard;
