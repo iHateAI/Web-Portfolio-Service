159218d49392205bf08af5eb1fa6bd00a7aea54b
dlzagu <eodnsdlekd@naver.com>
2022-08-25 02:03:47 +0900

    Fix : 제목 부제목 css변경

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Col, Row, Form, Button, Fade } from "react-bootstrap";
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
 
 import * as Api from "../../api";
 import { DispatchContext } from "../../App";
@@ -14,8 +14,6 @@ function LoginForm() {
   //useState로 password 상태를 생성함.
   const [password, setPassword] = useState("");
   // fade-up
-  const [fade, setFade] = useState(false);
-  const [fade2, setFade2] = useState(false);
 
   //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
@@ -61,84 +59,71 @@ function LoginForm() {
       console.log("로그인에 실패하였습니다.\n", err);
     }
   };
-  useEffect(() => {
-    setTimeout(() => setFade(true), 300);
-    setTimeout(() => setFade2(true), 600);
-  }, []);
+
   return (
     <section className="section pb-5">
+      <div className="login-content">
+        <h2 className="login-title">Login</h2>
+        <p className="login-sub-title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          necessitatibus incidunt ut officiis explicabo inventore.
+        </p>
+      </div>
       <Container>
-        <Row md={5} className="align-items-end">
-          <Fade in={fade}>
-            <Col md={6}>
-              <h2 className="login-title">Login</h2>
-              <p className="login-sub-title">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam necessitatibus incidunt ut officiis explicabo
-                inventore.
-              </p>
-            </Col>
-          </Fade>
-        </Row>
         <Row className="justify-content-md-center mt-5">
-          <Fade in={fade2}>
-            <Col lg={8}>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email Adress</Form.Label>
-                  <Form.Control
-                    type="email"
-                    autoComplete="on"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  {!isEmailValid && (
-                    <Form.Text className="text-success">
-                      이메일 형식이 올바르지 않습니다.
-                    </Form.Text>
-                  )}
-                </Form.Group>
+          <Col lg={8}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email Adress</Form.Label>
+                <Form.Control
+                  type="email"
+                  autoComplete="on"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {!isEmailValid && (
+                  <Form.Text className="text-success">
+                    이메일 형식이 올바르지 않습니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group controlId="formBasicPassword" className="mt-3">
-                  <Form.Label>password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    autoComplete="on"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {!isPasswordValid && (
-                    <Form.Text className="text-success">
-                      비밀번호는 4글자 이상입니다.
-                    </Form.Text>
-                  )}
-                </Form.Group>
+              <Form.Group controlId="formBasicPassword" className="mt-3">
+                <Form.Label>password</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="on"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {!isPasswordValid && (
+                  <Form.Text className="text-success">
+                    비밀번호는 4글자 이상입니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group as={Row} className="mt-3 text-center">
-                  <Col sm={{ span: 20 }}>
-                    <Button
-                      variant="primary"
-                      type="submit"
-                      disabled={!isFormValid}
-                    >
-                      로그인
-                    </Button>
-                  </Col>
-                </Form.Group>
+              <Form.Group as={Row} className="mt-3 text-center">
+                <Col sm={{ span: 20 }}>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    disabled={!isFormValid}
+                  >
+                    로그인
+                  </Button>
+                </Col>
+              </Form.Group>
 
-                <Form.Group as={Row} className="mt-3 text-center">
-                  <Col sm={{ span: 20 }}>
-                    <Button
-                      variant="light"
-                      onClick={() => navigate("/register")}
-                    >
-                      회원가입하기
-                    </Button>
-                  </Col>
-                </Form.Group>
-              </Form>
-            </Col>
-          </Fade>
+              <Form.Group as={Row} className="mt-3 text-center">
+                <Col sm={{ span: 20 }}>
+                  <Button variant="light" onClick={() => navigate("/register")}>
+                    회원가입하기
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </section>
