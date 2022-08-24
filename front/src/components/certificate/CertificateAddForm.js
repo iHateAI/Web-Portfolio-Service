20592553b46aea7d1baeffa80dd7d97e31e1ac0e
origin1508 <origin1508@gmail.com>
2022-08-24 23:14:51 +0900

    FEAT: CertificateAddForm, CertificateEditForm 유효성 검사 추가

--- front/src/components/certificate/CertificateAddForm.js
+++ front/src/components/certificate/CertificateAddForm.js
@@ -1,99 +1,103 @@
-import React, { useState } from "react"
-import { Button, Form, Col, Row } from "react-bootstrap"
-import DatePicker from "react-datepicker"
+import React, { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import DatePicker from "react-datepicker";
 import * as Api from "../../api";
+import useCertificateValidation from "../../hooks/useCertificateValidation";
 // import TestData from "../../dev/testData"
 
 function CertificateAddForm({
-    portfolioOwnerId,
-    setCertificates,
-    setIsAdding,
+  portfolioOwnerId,
+  setCertificates,
+  setIsAdding,
 }) {
-    const [title, setTitle] = useState("")
-    const [detail, setDetail] = useState("")
-    const [certificationDate, setCertificationDate] = useState(new Date())
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+  const [certificationDate, setCertificationDate] = useState(new Date());
+  const { checkValidateCertificateTitle, checkValidateCertificateDetail } =
+    useCertificateValidation();
 
-    const formatDate = (date) => {
-        const yyyy = date.getFullYear();
-        const mm = ("0" + (date.getMonth() + 1)).slice(-2);
-        const dd = ("0" + date.getDate()).slice(-2);
+  const formatDate = (date) => {
+    const yyyy = date.getFullYear();
+    const mm = ("0" + (date.getMonth() + 1)).slice(-2);
+    const dd = ("0" + date.getDate()).slice(-2);
 
-        return yyyy + '-' + mm + '-' + dd
-    }
+    return yyyy + "-" + mm + "-" + dd;
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const isValidTitle = checkValidateCertificateTitle(title);
+  const isValidDetail = checkValidateCertificateDetail(detail);
+  const isValidAll = isValidTitle && isValidDetail;
 
-        const user_id = portfolioOwnerId;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        await Api.post("api/certification", {
-            title,
-            detail,
-            certificationDate: formatDate(certificationDate)
-        });
+    if (!isValidAll) return;
 
-        const res = await Api.get("api/certification");
+    const user_id = portfolioOwnerId;
 
-        // const res = await TestData.createCertificate(TestData.userId, {
-        //     user_id: TestData.userId,
-        //     id: TestData[TestData.userId].certificates.length + 1,
-        //     title,
-        //     description,
-        //     certificateDate: formatDate(certificateDate),
-        // })
+    await Api.post("api/certification", {
+      title,
+      detail,
+      certificationDate: formatDate(certificationDate),
+    });
 
-        // setCertificates(res)
+    const res = await Api.get("api/certification");
 
-        setCertificates(res.data)
+    // const res = await TestData.createCertificate(TestData.userId, {
+    //     user_id: TestData.userId,
+    //     id: TestData[TestData.userId].certificates.length + 1,
+    //     title,
+    //     description,
+    //     certificateDate: formatDate(certificateDate),
+    // })
 
-        setIsAdding(false)
-    }
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="certificateAddTitle">
-                <Form.Control
-                    type="text"
-                    value={title}
-                    placeholder="자격증 제목"
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group controlId="certificateAddDetail" className="mt-3">
-                <Form.Control
-                    type="text"
-                    value={detail}
-                    placeholder="상세내역"
-                    onChange={(e) => setDetail(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group
-                as={Row}
-                controlId="certificateAddDate"
-                className="mt-3"
-            >
-                <Col xs="auto">
-                    <DatePicker
-                        selected={certificationDate}
-                        onChange={(date) => setCertificationDate(date)}
-                        dateFormat="yyyy-MM-dd"
-                    />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mt-3 text-center mb-4">
-                <Col sm={{ span: 20 }}>
-                    <Button variant="primary" type="submit" className="me-3">
-                        확인
-                    </Button>
-                    <Button
-                        variant="secondary"
-                        onClick={() => setIsAdding(false)}
-                    >
-                        취소
-                    </Button>
-                </Col>
-            </Form.Group>
-        </Form>
-    )
+    // setCertificates(res)
+
+    setCertificates(res.data);
+
+    setIsAdding(false);
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="certificateAddTitle">
+        <Form.Control
+          type="text"
+          value={title}
+          placeholder="자격증 제목"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        {!isValidTitle && <Form.Text muted>2글자 이상 적어주세요</Form.Text>}
+      </Form.Group>
+      <Form.Group controlId="certificateAddDetail" className="mt-3">
+        <Form.Control
+          type="text"
+          value={detail}
+          placeholder="상세내역"
+          onChange={(e) => setDetail(e.target.value)}
+        />
+        {!isValidDetail && <Form.Text muted>2글자 이상 적어주세요</Form.Text>}
+      </Form.Group>
+      <Form.Group as={Row} controlId="certificateAddDate" className="mt-3">
+        <Col xs="auto">
+          <DatePicker
+            selected={certificationDate}
+            onChange={(date) => setCertificationDate(date)}
+            dateFormat="yyyy-MM-dd"
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mt-3 text-center mb-4">
+        <Col sm={{ span: 20 }}>
+          <Button variant="primary" type="submit" className="me-3">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsAdding(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
 }
 
-export default CertificateAddForm
+export default CertificateAddForm;
--- front/src/util/validation/validation.js
+++ front/src/util/validation/validation.js
@@ -1,20 +1,34 @@
 class Validation {
-    // Check validation form
+  // Check validation form
 
-    // Award
-    // award의 title과 description의 길이는 앞 뒤 공백 제거 기준 5글자 이상이어야 한다.
-    static validateAwardTitle(title) {
-        const checkTitle = title.trim()
-        if (checkTitle.length < 5) return false
-        return true
-    }
+  // Award
+  // award의 title과 description의 길이는 앞 뒤 공백 제거 기준 5글자 이상이어야 한다.
+  static validateAwardTitle(title) {
+    const checkTitle = title.trim();
+    if (checkTitle.length < 5) return false;
+    return true;
+  }
 
-    static validateAwardDescription(description) {
-        const checkDescription = description.trim()
-        if (checkDescription.length < 5) return false
-        return true
-    }
+  static validateAwardDescription(description) {
+    const checkDescription = description.trim();
+    if (checkDescription.length < 5) return false;
+    return true;
+  }
+
+  // Certificate
+  // Certificate의 title, detail은 2글자 이상이어야 한다.
+  static validateCertificateTitle(title) {
+    const checkTitle = title.trim();
+    if (checkTitle.length < 2) return false;
+    return true;
+  }
+
+  static validateCertificateDetail(detail) {
+    const checkDetail = detail.trim();
+    if (checkDetail.length < 2) return false;
+    return true;
+  }
 }
 
-Object.freeze(Validation)
-export default Validation
+Object.freeze(Validation);
+export default Validation;
