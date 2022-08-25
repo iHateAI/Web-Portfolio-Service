392409ad5d2e704e73d8eeb6b94a8ffcbc58b3ac
DaHyunOhe <peelsob4d@gmail.com>
2022-08-26 04:16:08 +0900

    FEAT: Login/Register에 useForm을 통한 검증 추가, 로직 간소화

--- front/src/hooks/useForm.js
+++ front/src/hooks/useForm.js
@@ -2,25 +2,25 @@ import { useCallback, useEffect, useState } from 'react';
 import testValidate from '../util/validation/testValidate';
 
 export const useForm = (initialState) => {
-    const [values, setValues] = useState(initialState);
-    const [isValid, setIsValid] = useState({ all: false });
+  const [values, setValues] = useState(initialState);
+  const [isValid, setIsValid] = useState({ all: false });
 
-    const handleChange = useCallback(
-        (e, date) => {
-            if (date) {
-                const name = 'certificationDate';
-                setValues({ ...values, [name]: date });
-                return;
-            }
-            const { name, value } = e.target;
-            setValues({ ...values, [name]: value });
-        },
-        [values]
-    );
+  const handleChange = useCallback(
+    (e, date) => {
+      if (date) {
+        const name = 'certificationDate';
+        setValues({ ...values, [name]: date });
+        return;
+      }
+      const { name, value } = e.target;
+      setValues({ ...values, [name]: value });
+    },
+    [values]
+  );
 
-    useEffect(() => {
-        setIsValid(testValidate(values));
-    }, [values]);
+  useEffect(() => {
+    setIsValid(testValidate(values));
+  }, [values]);
 
-    return [values, isValid, handleChange];
+  return [values, isValid, handleChange];
 };
