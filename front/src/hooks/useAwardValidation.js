27bac63995c79cc155cae5f2c359a0a0a35ce1a7
DaHyunOhe <peelsob4d@gmail.com>
2022-08-25 11:00:20 +0900

    FIX: useAwardValidation 복구

--- front/src/hooks/useAwardValidation.js
+++ front/src/hooks/useAwardValidation.js
@@ -1,23 +1,23 @@
-import Validation from "../util/validation/validation"
+import Validation from '../util/validation/validation';
 
 const useAwardValidation = () => {
     const checkValidationTitle = (title) => {
-        return Validation.validateAwardTitle(title)
-    }
+        return Validation.validateAwardTitle(title);
+    };
 
     const checkValidationDescription = (description) => {
-        return Validation.validateAwardDescription(description)
-    }
+        return Validation.validateAwardDescription(description);
+    };
 
     const checkValidationAll = (title, description) => {
-        return [title, description].every((v) => v === true)
-    }
+        return [title, description].every((v) => v === true);
+    };
 
     return [
         checkValidationTitle,
         checkValidationDescription,
         checkValidationAll,
-    ]
-}
+    ];
+};
 
-export default useAwardValidation
+export default useAwardValidation;
