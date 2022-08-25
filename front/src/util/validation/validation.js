12e2a3621b577a64e7375776a10c73107e44aabe
foxmon <jjockrod@naver.com>
2022-08-25 10:54:48 +0900

    Fix: confilict 수정

--- front/src/util/validation/validation.js
+++ front/src/util/validation/validation.js
@@ -8,11 +8,13 @@ class Validation {
         return true;
     }
 
-    static validateAwardDescription(description) {
-        const checkDescription = description.trim();
-        if (checkDescription.length < 5) return false;
+    static validateAwardDetail(detail) {
+        const checkDetail = detail.trim();
+        if (checkDetail.length < 5) return false;
         return true;
     }
+
+    // Education
     static validateEducatuinUniversity(university) {
         const checkUniversity = university.trim();
         if (checkUniversity.length < 4) return false;
@@ -30,9 +32,36 @@ class Validation {
         if (checkStatus.length < 1) return false;
         return true;
     }
-    static validateAwardDetail(detail) {
+
+    // User
+    // 사용자의 비밀번호는 공백을(" ") 포함하면 안된다.
+    // 사용자의 비밀번호의 길이는 5글자 이상이어야 한다.
+    // 사용자의 비밀번호는 영문, 숫자 포함 6 ~ 12까지의 길이이다.
+    static validateUserEmail(email) {
+        return email
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    }
+
+    static validateUserPassword(password) {
+        if (password.includes(" ")) return false;
+        if (password.length < 5) return false;
+        return password.match(/^[A-Za-z0-9]{6,12}$/);
+    }
+
+    // Certificate
+    // Certificate의 title, detail은 2글자 이상이어야 한다.
+    static validateCertificateTitle(title) {
+        const checkTitle = title.trim();
+        if (checkTitle.length < 2) return false;
+        return true;
+    }
+
+    static validateCertificateDetail(detail) {
         const checkDetail = detail.trim();
-        if (checkDetail.length < 5) return false;
+        if (checkDetail.length < 2) return false;
         return true;
     }
 }
