401ee0d3e1d728e52c3cb7a6ef3b8df63202371a
foxmon <jjockrod@naver.com>
2022-08-26 09:54:27 +0900

    Fix: Portfolio 필요없는 import 수정

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -1,7 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Container, Col, Row } from "react-bootstrap";
-
 import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
