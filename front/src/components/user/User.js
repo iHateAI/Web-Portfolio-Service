09441323681f054489d9de518a2087cb166cb5c0
foxmon <jjockrod@naver.com>
2022-08-26 10:35:10 +0900

    Fix: User수정

--- front/src/components/user/User.js
+++ front/src/components/user/User.js
@@ -1,43 +1,41 @@
 import React, { useState, useEffect } from "react";
 import UserEditForm from "./UserEditForm";
-// import UserCard from "./UserCard";
 import UserCard2 from "./UserCard2";
 import * as Api from "../../api";
 
 function User({ portfolioOwnerId, isEditable }) {
-    // useState 훅을 통해 isEditing 상태를 생성함.
-    const [isEditing, setIsEditing] = useState(false);
-    // useState 훅을 통해 user 상태를 생성함.
-    const [user, setUser] = useState(null);
+  // useState 훅을 통해 isEditing 상태를 생성함.
+  const [isEditing, setIsEditing] = useState(false);
+  // useState 훅을 통해 user 상태를 생성함.
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
-        Api.get("users", portfolioOwnerId).then((res) => {
-            const data = res.data;
-            data.profileUrl =
-                data.profileUrl ||
-                `${process.env.PUBLIC_URL}/images/profile.PNG`;
-            setUser(data);
-        });
-    }, [portfolioOwnerId]);
+  useEffect(() => {
+    // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
+    Api.get("users", portfolioOwnerId).then((res) => {
+      const data = res.data;
+      data.profileUrl =
+        data.profileUrl || `${process.env.PUBLIC_URL}/images/profile.PNG`;
+      setUser(data);
+    });
+  }, [portfolioOwnerId]);
 
-    return (
-        <>
-            {isEditing ? (
-                <UserEditForm
-                    user={user}
-                    setIsEditing={setIsEditing}
-                    setUser={setUser}
-                />
-            ) : (
-                <UserCard2
-                    user={user}
-                    setIsEditing={setIsEditing}
-                    isEditable={isEditable}
-                />
-            )}
-        </>
-    );
+  return (
+    <>
+      {isEditing ? (
+        <UserEditForm
+          user={user}
+          setIsEditing={setIsEditing}
+          setUser={setUser}
+        />
+      ) : (
+        <UserCard2
+          user={user}
+          setIsEditing={setIsEditing}
+          isEditable={isEditable}
+        />
+      )}
+    </>
+  );
 }
 
 export default User;
