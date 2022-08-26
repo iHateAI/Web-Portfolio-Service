4bd1c698c2b176564fb7ad637691d9423f377c59
foxmon <jjockrod@naver.com>
2022-08-26 10:20:59 +0900

    Fix: UserEditForm 수정

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -5,237 +5,243 @@ import { Form } from "react-bootstrap";
 import AlertModal from "../modal/AlertModal";
 import * as Api from "../../api";
 import {
-    varColors,
-    varFontSize,
-    varFontWeight,
-    varLineHeight,
-    varSpacing,
+  varColors,
+  varFontSize,
+  varFontWeight,
+  varLineHeight,
+  varSpacing,
 } from "../../util/theme/theme";
 
 const UserEditForm2 = ({ user, setIsEditing, setUser }) => {
-    const [name, setName] = useState(user.name);
-    const [email, setEmail] = useState(user.email);
-    const [description, setDescription] = useState(user.description);
-    const [imagePreview, setImagePreview] = useState(user.profileUrl);
-    const [uploadedImg, setUploadedImg] = useState(null);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [description, setDescription] = useState(user.description);
+  const [imagePreview, setImagePreview] = useState(user.profileUrl);
+  const [uploadedImg, setUploadedImg] = useState(null);
 
-    const [
-        isShow,
-        onShowButtonClickEventHandler,
-        onCloseButtonClickEventHandler,
-    ] = useModal(false);
-    const [checkValidationEmail, _, checkValidationDescription] =
-        useUserValidation();
+  const [
+    isShow,
+    onShowButtonClickEventHandler,
+    onCloseButtonClickEventHandler,
+  ] = useModal(false);
+  const [checkValidationEmail, _, checkValidationDescription] =
+    useUserValidation();
 
-    const isValidEmail = checkValidationEmail(email);
-    const isValidDescription = checkValidationDescription(description);
-    const isValid = isValidEmail && isValidDescription;
+  const isValidEmail = checkValidationEmail(email);
+  const isValidDescription = checkValidationDescription(description);
+  const isValid = isValidEmail && isValidDescription;
 
-    const acceptedFile = ["image/jpg", "image/png", "image/jpeg"];
+  const acceptedFile = ["image/jpg", "image/png", "image/jpeg"];
 
-    const hanlderNameChange = useCallback((e) => {
-        setName(e.target.value);
-    }, []);
+  const hanlderNameChange = useCallback((e) => {
+    setName(e.target.value);
+  }, []);
 
-    const handlerEmailChange = useCallback((e) => {
-        setEmail(e.target.value);
-    }, []);
+  const handlerEmailChange = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
 
-    const handlerDescriptionChange = useCallback((e) => {
-        setDescription(e.target.value);
-    }, []);
+  const handlerDescriptionChange = useCallback((e) => {
+    setDescription(e.target.value);
+  }, []);
 
-    const handlerImageUpload = (e) => {
-        setUploadedImg(e.target.files[0]);
-        const preview = new FileReader();
-        preview.readAsDataURL(e.target.files[0]);
-        preview.onload = () => {
-            setImagePreview(preview.result);
-        };
+  const handlerImageUpload = (e) => {
+    setUploadedImg(e.target.files[0]);
+    const preview = new FileReader();
+    preview.readAsDataURL(e.target.files[0]);
+    preview.onload = () => {
+      setImagePreview(preview.result);
     };
+  };
 
-    const handleSubmitClick = async (e) => {
-        e.preventDefault();
-        if (!isValid) {
-            onShowButtonClickEventHandler();
-            return;
-        }
-        if (!uploadedImg) {
-            const res = await Api.put(`users/${user.id}`, {
-                name,
-                email,
-                description,
-            });
-            const updatedUser = res.data;
-            setUser({
-                ...updatedUser,
-                profileUrl: user?.profileUrl,
-            });
-        } else {
-            const formData = new FormData();
-            formData.append("image", uploadedImg);
-            const res = await Api.imageUpload(`users/${user.id}`, formData);
-            const updatedUser = res.data;
-            console.log(updatedUser);
-            setUser(updatedUser);
-        }
-        setIsEditing(false);
-    };
+  const handleSubmitClick = async (e) => {
+    e.preventDefault();
+    if (!isValid) {
+      onShowButtonClickEventHandler();
+      return;
+    }
 
-    const handlerCancelClick = () => {
-        setIsEditing(false);
-    };
+    // 뭔가 이미지의 경우 image수정 버튼을 따로 만들어서 profile update랑 분리를 해야할 것 같다.
+    const userObj = { name, email, description, id: user.id };
+    if (!uploadedImg) {
+      const updatedUser = await fetchUpdateUserInformation.call(this, userObj);
+      setUser({
+        ...updatedUser,
+        profileUrl: user?.profileUrl,
+      });
+    } else {
+      const updatedUser = await fetchUpdaeUserImage.call(
+        this,
+        uploadedImg,
+        userObj.id
+      );
+      setUser(updatedUser);
+    }
+    setIsEditing(false);
+  };
 
-    return (
-        <div style={containerStyle}>
-            <form style={formStyle}>
-                <div style={titleStyle}>EDIT USER INFORMATION</div>
-                <input
-                    type="text"
-                    placeholder="Name..."
-                    style={inputStyle}
-                    value={name}
-                    onChange={hanlderNameChange}
-                />
-                <input
-                    type="email"
-                    placeholder="Email..."
-                    style={inputStyle}
-                    value={email}
-                    onChange={handlerEmailChange}
-                />
-                {!isValidEmail && (
-                    <Form.Text className="text-danger">
-                        Please check your email.
-                    </Form.Text>
-                )}
-                <input
-                    type="text"
-                    placeholder="Description..."
-                    style={inputStyle}
-                    value={description}
-                    onChange={handlerDescriptionChange}
-                />
-                {!isValidDescription && (
-                    <Form.Text className="text-danger">
-                        Please check your description.
-                    </Form.Text>
-                )}
-                <label htmlFor="image-input" style={imageLabelStyle}>
-                    Image upload
-                </label>
-                <input
-                    type="file"
-                    id="image-input"
-                    accept={acceptedFile.join(", ")}
-                    name="image"
-                    style={imageInputStyle}
-                    onChange={handlerImageUpload}
-                />
-                {imagePreview && (
-                    <div style={previewWrapperStyle}>
-                        <img
-                            style={previewStyle}
-                            src={imagePreview}
-                            alt="profile-image"
-                        />{" "}
-                    </div>
-                )}
-                <button
-                    type="submit"
-                    style={buttonStyle}
-                    onClick={handleSubmitClick}
-                >
-                    CONFIRM
-                </button>
-                <button style={buttonCancelStyle} onClick={handlerCancelClick}>
-                    CANCEL
-                </button>
-            </form>
-            <AlertModal
-                msg="Please check your information."
-                isShow={isShow}
-                onCloseButtonClickEvent={onCloseButtonClickEventHandler}
-            />
-        </div>
-    );
+  const handlerCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div style={containerStyle}>
+      <form style={formStyle}>
+        <div style={titleStyle}>EDIT USER INFORMATION</div>
+        <input
+          type="text"
+          placeholder="Name..."
+          style={inputStyle}
+          value={name}
+          onChange={hanlderNameChange}
+        />
+        <input
+          type="email"
+          placeholder="Email..."
+          style={inputStyle}
+          value={email}
+          onChange={handlerEmailChange}
+        />
+        {!isValidEmail && (
+          <Form.Text className="text-danger">
+            Please check your email.
+          </Form.Text>
+        )}
+        <input
+          type="text"
+          placeholder="Description..."
+          style={inputStyle}
+          value={description}
+          onChange={handlerDescriptionChange}
+        />
+        {!isValidDescription && (
+          <Form.Text className="text-danger">
+            Please check your description.
+          </Form.Text>
+        )}
+        <label htmlFor="image-input" style={imageLabelStyle}>
+          Image upload
+        </label>
+        <input
+          type="file"
+          id="image-input"
+          accept={acceptedFile.join(", ")}
+          name="image"
+          style={imageInputStyle}
+          onChange={handlerImageUpload}
+        />
+        {imagePreview && (
+          <div style={previewWrapperStyle}>
+            <img style={previewStyle} src={imagePreview} alt="profile-image" />{" "}
+          </div>
+        )}
+        <button type="submit" style={buttonStyle} onClick={handleSubmitClick}>
+          CONFIRM
+        </button>
+        <button style={buttonCancelStyle} onClick={handlerCancelClick}>
+          CANCEL
+        </button>
+      </form>
+      <AlertModal
+        msg="Please check your information."
+        isShow={isShow}
+        onCloseButtonClickEvent={onCloseButtonClickEventHandler}
+      />
+    </div>
+  );
 };
 
+async function fetchUpdateUserInformation(user) {
+  const res = await Api.put(`users/${user.id}`, {
+    name: user.name,
+    email: user.email,
+    description: user.description,
+  });
+  return res.data;
+}
+
+async function fetchUpdaeUserImage(uploadedImg, user) {
+  const formData = new FormData();
+  formData.append("image", uploadedImg);
+  const res = await Api.imageUpload(`users/${user.id}`, formData);
+  return res.data;
+}
+
 const containerStyle = {
-    width: "100%",
+  width: "100%",
 };
 
 const titleStyle = {
-    fontSize: varFontSize.bp1024(5),
-    lineHeight: varLineHeight.bp1024(8),
-    fontWeight: varFontWeight.bold,
-    textAlign: "center",
-    marginBottom: "10px",
+  fontSize: varFontSize.bp1024(5),
+  lineHeight: varLineHeight.bp1024(8),
+  fontWeight: varFontWeight.bold,
+  textAlign: "center",
+  marginBottom: "10px",
 };
 
 const formStyle = {
-    margin: "auto",
-    color: varColors.light.coolBlack,
-    fontFamily: "system-ui",
+  margin: "auto",
+  color: varColors.light.coolBlack,
+  fontFamily: "system-ui",
 };
 
 const inputStyle = {
-    backgroundColor: varColors.light.coolWhiteGray,
-    width: "100%",
-    height: varSpacing.bp1024(5),
-    outline: "none",
-    border: "0px",
-    fontSize: varFontSize.bp1024(4),
-    lineHeight: varLineHeight.bp1024(4),
-    padding: varSpacing.bp1024(1),
-    borderRadius: "4px",
-    "&:hover": {
-        backgroundColor: varColors.light.coolLightGray,
-    },
-    marginBottom: "10px",
+  backgroundColor: varColors.light.coolWhiteGray,
+  width: "100%",
+  height: varSpacing.bp1024(5),
+  outline: "none",
+  border: "0px",
+  fontSize: varFontSize.bp1024(4),
+  lineHeight: varLineHeight.bp1024(4),
+  padding: varSpacing.bp1024(1),
+  borderRadius: "4px",
+  "&:hover": {
+    backgroundColor: varColors.light.coolLightGray,
+  },
+  marginBottom: "10px",
 };
 
 const imageLabelStyle = {
-    width: "100%",
-    backgroundColor: varColors.light.coolLightGray,
-    color: varColors.light.coolBlack,
-    border: "0px",
-    borderRadius: "4px",
-    marginBottom: "10px",
-    padding: varSpacing.bp1024(1),
-    cursor: "pointer",
+  width: "100%",
+  backgroundColor: varColors.light.coolLightGray,
+  color: varColors.light.coolBlack,
+  border: "0px",
+  borderRadius: "4px",
+  marginBottom: "10px",
+  padding: varSpacing.bp1024(1),
+  cursor: "pointer",
 };
 
 const imageInputStyle = {
-    display: "none",
+  display: "none",
 };
 
 const previewWrapperStyle = {
-    textAlign: "center",
+  textAlign: "center",
 };
 
 const previewStyle = {
-    width: "30%",
-    height: "30%",
+  width: "30%",
+  height: "30%",
 };
 
 const buttonStyle = {
-    width: "100%",
-    backgroundColor: varColors.light.lightSky,
-    color: varColors.light.white,
-    border: "0px",
-    borderRadius: "4px",
-    marginBottom: "10px",
-    padding: varSpacing.bp1024(1),
+  width: "100%",
+  backgroundColor: varColors.light.lightSky,
+  color: varColors.light.white,
+  border: "0px",
+  borderRadius: "4px",
+  marginBottom: "10px",
+  padding: varSpacing.bp1024(1),
 };
 
 const buttonCancelStyle = {
-    width: "100%",
-    backgroundColor: varColors.light.sky,
-    color: varColors.light.white,
-    border: "0px",
-    borderRadius: "4px",
-    padding: varSpacing.bp1024(1),
+  width: "100%",
+  backgroundColor: varColors.light.sky,
+  color: varColors.light.white,
+  border: "0px",
+  borderRadius: "4px",
+  padding: varSpacing.bp1024(1),
 };
 
 export default UserEditForm2;
