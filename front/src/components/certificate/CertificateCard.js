7e7f5d4f8f0cbb0f06c9ae03286500868b136bdd
DaHyunOhe <peelsob4d@gmail.com>
2022-08-26 22:27:13 +0900

    FIX: Certificate, Project MVP css적용 및 Portfolio에 추가, API 수정

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -3,49 +3,49 @@ import useModal from "../../hooks/useModal";
 import ConfirmModal from "../modal/ConfirmModal";
 
 function CertificateCard({ certificate, isEditable, setIsEditing }) {
-    const [ 
-        isShow,
-        onShowButtonClickEventHandler,
-        onCloseButtonClickEventHandler,
-    ] = useModal(false)
+  const [
+    isShow,
+    onShowButtonClickEventHandler,
+    onCloseButtonClickEventHandler,
+  ] = useModal(false);
 
-    return (
-        <Card.Text>
-            <Row className="align-items-center">
-                <Col>
-                    <span>{certificate.title}</span>
-                    <br />
-                    <span className="text-muted">{certificate.detail}</span>
-                    <br />
-                    <span className="text-muted">{(certificate.certificationDate).split("T")[0]}</span>
-                </Col>
-                {isEditable && (
-                    <Col xs="4" className="text-end">
-                        <Button
-                            className="me-3"
-                            variant="outline-info"
-                            size='sm'
-                            onClick={() => setIsEditing(true)}
-                        >편집</Button>
-                        <Button
-                            variant="danger"
-                            size='sm'
-                            onClick={onShowButtonClickEventHandler}
-                        >삭제</Button>
-                    </Col>
-                )}
-            </Row>
-            { isShow && (
-                <ConfirmModal
-                    msg="정말 삭제하시겠습니까?"
-                    isShow={isShow}
-                    onCloseButtonClickEvent={onCloseButtonClickEventHandler}
-                    onCheckButtonClickEvent={onCloseButtonClickEventHandler}
-                />
-            )}
-            
-        </Card.Text>
-    );
+  return (
+    <Card.Text>
+      <div className="mvp-content-detail">
+        <div className="mvp-info">
+          <h3 className="title">{certificate.title}</h3>
+          <p className="sub-title">{certificate.detail}</p>
+          <p className="sub-title">
+            {certificate.certificationDate.split("T")[0]}
+          </p>
+        </div>
+        {isEditable && (
+          <div className="mvp-management">
+            <button
+              className="mvp-edit-button"
+              onClick={() => setIsEditing(true)}
+            >
+              편집
+            </button>
+            <button
+              className="mvp-delete-button"
+              onClick={onShowButtonClickEventHandler}
+            >
+              삭제
+            </button>
+          </div>
+        )}
+      </div>
+      {isShow && (
+        <ConfirmModal
+          msg="정말 삭제하시겠습니까?"
+          isShow={isShow}
+          onCloseButtonClickEvent={onCloseButtonClickEventHandler}
+          onCheckButtonClickEvent={onCloseButtonClickEventHandler}
+        />
+      )}
+    </Card.Text>
+  );
 }
 
 export default CertificateCard;
--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,66 +1,55 @@
-import React from 'react';
-import { Button, ButtonGroup, Card, Row } from 'react-bootstrap';
+import React from "react";
+import { Button, ButtonGroup, Card, Row } from "react-bootstrap";
 // import { deleteProject } from './dev/mockApiProject';
-import TestData from '../../dev/testData';
-import useModal from '../../hooks/useModal';
-import ConfirmModal from '../modal/ConfirmModal';
+import TestData from "../../dev/testData";
+import useModal from "../../hooks/useModal";
+import ConfirmModal from "../modal/ConfirmModal";
 
 function ProjectCard({ project, isEditable, editClick, getUser }) {
-    const [
-        isShow,
-        onShowButtonClickEventHandler,
-        onCloseButtonClickEventHandler,
-    ] = useModal(false);
+  const [
+    isShow,
+    onShowButtonClickEventHandler,
+    onCloseButtonClickEventHandler,
+  ] = useModal(false);
 
-    const handleDeleteProject = async (bool) => {
-        // await deleteProject(project.key);
-        await TestData.deleteProject(project._id);
-        getUser();
-    };
+  const handleDeleteProject = async (bool) => {
+    // await deleteProject(project.key);
+    await TestData.deleteProject(project._id);
+    getUser();
+  };
 
-    return (
-        <div className="d-flex justify-content-between align-items-center">
-            <Row>
-                <Card.Body>
-                    <Card.Title>{project.title}</Card.Title>
-                    <Card.Subtitle>{project.detail}</Card.Subtitle>
-                    <Card.Text>
-                        {project.startDate} ~ {project.endDate}
-                    </Card.Text>
-                </Card.Body>
-            </Row>
-            {isEditable && (
-                <ButtonGroup>
-                    <Button
-                        variant="outline-info"
-                        size="sm"
-                        style={{
-                            height: '30px',
-                        }}
-                        onClick={editClick}
-                    >
-                        편집
-                    </Button>
-                    <Button
-                        variant="outline-danger"
-                        size="sm"
-                        style={{
-                            height: '30px',
-                        }}
-                        onClick={onShowButtonClickEventHandler}
-                    >
-                        삭제
-                    </Button>
-                </ButtonGroup>
-            )}
-            <ConfirmModal
-                isShow={isShow}
-                onCloseButtonClickEvent={onCloseButtonClickEventHandler}
-                onCheckButtonClickEvent={handleDeleteProject}
-                msg={`${project.title}(을)를 목록에서 삭제하시겠습니까?`}
-            />
+  return (
+    <div className="mvp-content-detail">
+      <Row>
+        <div className="mvp-info">
+          <h3 className="title">{project.title}</h3>
+          <p className="sub-title">{project.detail}</p>
+          <p className="sub-title">
+            {project.startDate} ~ {project.endDate}
+          </p>
         </div>
-    );
+      </Row>
+      {isEditable && (
+        <div className="mvp-management">
+          <button className="mvp-edit-button" onClick={editClick}>
+            편집
+          </button>
+          <button
+            className="mvp-delete-button"
+            onClick={onShowButtonClickEventHandler}
+          >
+            삭제
+          </button>
+        </div>
+      )}
+      <ConfirmModal
+        isShow={isShow}
+        onCloseButtonClickEvent={onCloseButtonClickEventHandler}
+        onCheckButtonClickEvent={handleDeleteProject}
+        msg={`${project.title}(을)를 목록에서 삭제하시겠습니까?`}
+      />
+    </div>
+  );
 }
 
 export default ProjectCard;
