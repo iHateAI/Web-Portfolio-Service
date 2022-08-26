a6f0c0577a449cbc571170fe1581d1d44501de7f
foxmon <jjockrod@naver.com>
2022-08-26 10:36:35 +0900

    Fix: AwardCard 수정

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,44 +1,44 @@
 const AwardCard = ({
-    award,
-    isEditable,
-    onEnterEditModeButtonClickEvent,
-    onDeleteButtonClickEvent,
+  award,
+  isEditable,
+  onEnterEditModeButtonClickEvent,
+  onDeleteButtonClickEvent,
 }) => {
-    const onEnterEditModeButtonClickEventHandler = () => {
-        onEnterEditModeButtonClickEvent();
-    };
+  const onEnterEditModeButtonClickEventHandler = () => {
+    onEnterEditModeButtonClickEvent();
+  };
 
-    const onDeleteButtonClickEventHandler = () => {
-        onDeleteButtonClickEvent(award);
-    };
+  const onDeleteButtonClickEventHandler = () => {
+    onDeleteButtonClickEvent(award);
+  };
 
-    return (
-        <div className="mvp-content-detail">
-            <div className="mvp-info">
-                <h3 className="title">{award.title}</h3>
+  return (
+    <div className="mvp-content-detail">
+      <div className="mvp-info">
+        <h3 className="title">{award.title}</h3>
 
-                <p className="sub-title">
-                    <span>{award.detail}</span>
-                </p>
-            </div>
-            {isEditable && (
-                <div className="mvp-management">
-                    <button
-                        onClick={onEnterEditModeButtonClickEventHandler}
-                        className="mvp-edit-button"
-                    >
-                        edit
-                    </button>
-                    <button
-                        onClick={onDeleteButtonClickEventHandler}
-                        className="mvp-delete-button"
-                    >
-                        delete
-                    </button>
-                </div>
-            )}
+        <p className="sub-title">
+          <span>{award.detail}</span>
+        </p>
+      </div>
+      {isEditable && (
+        <div className="mvp-management">
+          <button
+            onClick={onEnterEditModeButtonClickEventHandler}
+            className="mvp-edit-button"
+          >
+            edit
+          </button>
+          <button
+            onClick={onDeleteButtonClickEventHandler}
+            className="mvp-delete-button"
+          >
+            delete
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default AwardCard;
