fd0086c203f0740684a58f785c9273d932ab6065
foxmon <jjockrod@naver.com>
2022-08-26 22:28:10 +0900

    Fix: validation nickname 추가

--- front/src/util/validation/testValidate.js
+++ front/src/util/validation/testValidate.js
@@ -1,35 +1,46 @@
 const testValidate = (values) => {
   const result = { all: true };
   for (const [name, value] of Object.entries(values)) {
-    if (name === '_id') continue;
+    if (name === "_id") continue;
     switch (name) {
-      case 'title':
-      case 'detail':
+      case "title":
+      case "detail":
         result[name] = validateLength(value, 5);
         break;
-      case 'university':
-      case 'major':
+
+      case "university":
+      case "major":
         result[name] = validateLength(value, 4);
         break;
-      case 'name':
+
+      case "name":
         result[name] = validateLength(value, 2);
         break;
-      case 'status':
-      case 'description':
+
+      case "status":
+      case "description":
         result[name] = validateLength(value, 1);
         break;
-      case 'startDate':
-      case 'endDate':
-      case 'certificationDate':
+
+      case "startDate":
+      case "endDate":
+      case "certificationDate":
         result[name] = validateDate(value);
         break;
-      case 'email':
+
+      case "email":
         result[name] = validateEmail(value);
         break;
-      case 'password':
-      case 'confirmPassword':
+
+      case "password":
+      case "confirmPassword":
         result[name] = validatePassword(value);
         break;
+
+      case "nickname":
+        result[name] = validateNickname(value);
+        break;
+
       default:
         break;
     }
@@ -48,14 +59,14 @@ const validateEmail = (email) => {
   const checkEmail = email
     .toLowerCase()
     .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
   if (checkEmail === null) return false;
   return true;
 };
 
 const validatePassword = (password) => {
-  if (password.includes(' ')) return false;
+  if (password.includes(" ")) return false;
   //if (password.length < 4) return false;
   const checkPassword = password.match(/^[A-Za-z0-9]{4,12}$/);
   if (checkPassword === null) return false;
@@ -63,8 +74,14 @@ const validatePassword = (password) => {
 };
 
 const validateDate = (date) => {
-  const checkDate = date.split('-');
+  const checkDate = date.split("-");
   if (checkDate.length !== 3) return false;
+  return true;
+};
+
+const validateNickname = (nickname) => {
+  const checkNickname = nickname.trim();
+  if (checkNickname.length < 2) return false;
   return true;
 };
 
