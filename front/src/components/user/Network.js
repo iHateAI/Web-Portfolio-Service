89ebe78cfc34a341b157a220255a5d23be473671
DaHyunOhe <peelsob4d@gmail.com>
2022-08-27 17:32:27 +0900

    FEAT: Network에서 유저 랜덤한 순서로 출력하도록 변경

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -1,11 +1,21 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Container, Row } from 'react-bootstrap';
+import React, { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
 
-import * as Api from '../../api';
+import * as Api from "../../api";
 // import UserCard from "./UserCard";
-import UserCard2 from './UserCard2';
-import { UserStateContext } from '../../App';
+import UserCard2 from "./UserCard2";
+import { UserStateContext } from "../../App";
+
+const shuffle = (array) => {
+  for (let currentIndex = array.length - 1; currentIndex > 0; currentIndex--) {
+    const targetIndex = Math.floor(Math.random() * (currentIndex + 1));
+    [array[currentIndex], array[targetIndex]] = [
+      array[targetIndex],
+      array[currentIndex],
+    ];
+  }
+};
 
 function Network() {
   const navigate = useNavigate();
@@ -16,11 +26,11 @@ function Network() {
   useEffect(() => {
     // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
     if (!userState.user) {
-      navigate('/login');
+      navigate("/login");
       return;
     }
     // "userlist" 엔드포인트로 GET 요청을 하고, users를 response의 data로 세팅함.
-    Api.get('userlist').then((res) => {
+    Api.get("userlist").then((res) => {
       const data = res.data;
       const userArr = [];
       data.forEach((v) => {
@@ -28,21 +38,22 @@ function Network() {
           v.profileUrl || `${process.env.PUBLIC_URL}/images/profile.PNG`;
         userArr.push({ ...v, profileUrl: image });
       });
+      shuffle(userArr);
       setUsers(userArr);
     });
   }, [userState, navigate]);
 
   return (
-    <div className='network-container'>
-      <section className='section network-content'>
-        <div className='title-container'>
-          <h2 className='network-title'>Hey, I'm Johan Stanworth</h2>
-          <p className='network-sub-title'>
+    <div className="network-container">
+      <section className="section network-content">
+        <div className="title-container">
+          <h2 className="network-title">Hey, I'm Johan Stanworth</h2>
+          <p className="network-sub-title">
             Freelance Creative &amp; Professional Graphics Designer
           </p>
         </div>
-        <div className='usercard-container'>
-          <div className='usercard'>
+        <div className="usercard-container">
+          <div className="usercard">
             {users.map((user) => (
               <UserCard2 key={user.id} user={user} isNetwork />
             ))}
