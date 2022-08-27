7928c857b52a88ac9fa970f063410e6040c43d98
origin1508 <origin1508@gmail.com>
2022-08-27 14:17:47 +0900

    FIX: UserCard2 수정
    BookmakrButton import

--- front/src/components/user/UserCard2.js
+++ front/src/components/user/UserCard2.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../style/app.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/app.css";
+import BookmarkButton from "../bookmark/BookmarkButton";
 
 function UserCard2({ user, setIsEditing, isEditable, isNetwork }) {
   const navigate = useNavigate();
@@ -19,12 +20,12 @@ function UserCard2({ user, setIsEditing, isEditable, isNetwork }) {
   // jsx 부분의 코드 길이가 길어지므로 이렇게 사용.
   const UserInformation = () => {
     return (
-      <div className='network-item-info'>
-        <h3 className='user-title'>{user?.name}</h3>
-        <h3 className='user-email'>{user?.email}</h3>
-        <span className='user-descriptiom'>{user?.description}</span>
+      <div className="network-item-info">
+        <h3 className="user-title">{user?.name}</h3>
+        <h3 className="user-email">{user?.email}</h3>
+        <span className="user-descriptiom">{user?.description}</span>
         {isEditable && (
-          <button className='button-style' onClick={handlerEditClick}>
+          <button className="button-style" onClick={handlerEditClick}>
             EDIT
           </button>
         )}
@@ -33,16 +34,17 @@ function UserCard2({ user, setIsEditing, isEditable, isNetwork }) {
   };
 
   return (
-    <div className='item-box'>
-      <div className='item-wrap' onClick={handlerPortfolioClick}>
+    <div className="item-box">
+      <div className="item-wrap" onClick={handlerPortfolioClick}>
         <UserInformation />
         <img
-          className='item-img-style'
+          className="item-img-style"
           // src='https://cdn.pixabay.com/photo/2018/01/10/23/53/rabbit-3075088_1280.png'
           src={user.profileUrl}
-          alt='userImg'
+          alt="userImg"
         />
       </div>
+      <BookmarkButton user={user} />
     </div>
   );
 }
