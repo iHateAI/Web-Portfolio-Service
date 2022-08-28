d7e5a21cd681e764bc72a860f7e21a9a9e05e006
origin1508 <origin1508@gmail.com>
2022-08-28 12:44:37 +0900

    FIX: CertificateCard 수정
    Warning: validateDOMNesting(...): <div> cannot appear as a descendant of <p>에러 해결

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -10,32 +10,30 @@ function CertificateCard({ certificate, isEditable, setIsEditing }) {
   ] = useModal(false);
 
   return (
-    <Card.Text>
-      <div className="mvp-content-detail">
-        <div className="mvp-info">
-          <h3 className="title">{certificate.title}</h3>
-          <p className="sub-title">{certificate.detail}</p>
-          <p className="sub-title">
-            {certificate.certificationDate.split("T")[0]}
-          </p>
-        </div>
-        {isEditable && (
-          <div className="mvp-management">
-            <button
-              className="mvp-edit-button"
-              onClick={() => setIsEditing(true)}
-            >
-              편집
-            </button>
-            <button
-              className="mvp-delete-button"
-              onClick={onShowButtonClickEventHandler}
-            >
-              삭제
-            </button>
-          </div>
-        )}
+    <div className="mvp-content-detail">
+      <div className="mvp-info">
+        <h3 className="title">{certificate.title}</h3>
+        <p className="sub-title">{certificate.detail}</p>
+        <p className="sub-title">
+          {certificate.certificationDate.split("T")[0]}
+        </p>
       </div>
+      {isEditable && (
+        <div className="mvp-management">
+          <button
+            className="mvp-edit-button"
+            onClick={() => setIsEditing(true)}
+          >
+            편집
+          </button>
+          <button
+            className="mvp-delete-button"
+            onClick={onShowButtonClickEventHandler}
+          >
+            삭제
+          </button>
+        </div>
+      )}
       {isShow && (
         <ConfirmModal
           msg="정말 삭제하시겠습니까?"
@@ -44,7 +42,7 @@ function CertificateCard({ certificate, isEditable, setIsEditing }) {
           onCheckButtonClickEvent={onCloseButtonClickEventHandler}
         />
       )}
-    </Card.Text>
+    </div>
   );
 }
 
