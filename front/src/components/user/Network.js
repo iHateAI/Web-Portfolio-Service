b19bb8e2ddaa18b56690d62b394bffa0815b32c4
origin1508 <origin1508@gmail.com>
2022-08-29 13:06:25 +0900

    FIX: Network 수정
    bookmarks 선택시 bookmarkslist가 나오도록 탭 추가함

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -3,13 +3,14 @@ import { useNavigate } from "react-router-dom";
 import * as Api from "../../api";
 import UserCard2 from "./UserCard2";
 import { UserStateContext } from "../../App";
+import BookmarksList from "../bookmark/BookmarksList";
 
 function Network() {
   const navigate = useNavigate();
   const userState = useContext(UserStateContext);
 
   const [users, setUsers] = useState([]);
-
+  const [selectedTab, setSelectedTab] = useState("all");
   useEffect(() => {
     if (!userState.user) {
       navigate("/login");
@@ -36,11 +37,33 @@ function Network() {
             Freelance Creative &amp; Professional Graphics Designer
           </p>
         </div>
+        <div className="tabs-container" style={tabMenuStyle}>
+          <li
+            id="all"
+            className={selectedTab === "all" ? "tab active" : "tab"}
+            onClick={(e) => setSelectedTab(e.target.id)}
+            style={selectedTab === "all" ? activeTabStyle : tabStyle}
+          >
+            All users
+          </li>
+          <li
+            id="bookmarks"
+            className={selectedTab === "bookmarks" ? "tab active" : "tab"}
+            onClick={(e) => setSelectedTab(e.target.id)}
+            style={selectedTab === "bookmarks" ? activeTabStyle : tabStyle}
+          >
+            Bookmarks
+          </li>
+        </div>
         <div className="usercard-container">
           <div className="usercard">
-            {users.map((user) => (
-              <UserCard2 key={user.id} user={user} isNetwork />
-            ))}
+            {selectedTab === "all" ? (
+              users.map((user) => (
+                <UserCard2 key={user.id} user={user} isNetwork />
+              ))
+            ) : (
+              <BookmarksList users={users} />
+            )}
           </div>
         </div>
       </section>
@@ -49,3 +72,33 @@ function Network() {
 }
 
 export default Network;
+
+const tabMenuStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  width: "100%",
+  listStyle: "none",
+  textAlign: "center",
+  fontSize: "2rem",
+  fontFamily: "Raleway",
+};
+
+const tabStyle = {
+  flex: 1,
+  height: "40px",
+  lineHeight: "40px",
+  backgroundColor: "#f2f2f3",
+  borderBottom: "3px solid #0d1e2d",
+  transition: "all 0.3s",
+};
+
+const activeTabStyle = {
+  flex: 1,
+  height: "40px",
+  lineHeight: "40px",
+  fontWeight: "bold",
+  backgroundColor: "#0d1e2d",
+  borderBottom: "3px solid #0d1e2d",
+  color: "white",
+  transition: "all 0.3s",
+};
