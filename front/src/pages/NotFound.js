0b28b817b9d79752bb93421a7b24547637858136
foxmon <jjockrod@naver.com>
2022-08-31 12:03:45 +0900

    Feat: NotFound 제작

---
+++ front/src/pages/NotFound.js
@@ -0,0 +1,44 @@
+const NotFound = () => {
+  return (
+    <div style={containerStyle}>
+      <div>
+        <h1 style={fontStyle}>
+          <span style={spanStyle}>4</span>
+          <span style={spanStyle}>0</span>
+          <span style={spanStyle}>4</span>
+        </h1>
+      </div>
+      <h2 style={contentStyle}>
+        we are sorry, but the page you requested was not found
+      </h2>
+    </div>
+  );
+};
+
+const containerStyle = {
+  width: "100%",
+  lineHeight: 1.4,
+  textAlign: "center",
+};
+
+const fontStyle = {
+  color: "#262626",
+  letterSpacing: "-40px",
+  marginLeft: "-20px",
+  fontSize: "250px",
+  fontWeight: 900,
+};
+
+const spanStyle = {
+  textShadow: "-8px 0px 0px #fff",
+};
+
+const contentStyle = {
+  fontFamily: "Cabin",
+  fontSize: "20px",
+  fontWeight: 700,
+  textTransform: "uppercase",
+  color: "#000",
+};
+
+export default NotFound;
